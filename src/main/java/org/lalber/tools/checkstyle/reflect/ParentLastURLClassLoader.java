55c31092021495743bd414b647bff7e55495aa81
Lucas Alber <lucasd.alber@gmail.com>
2019-12-15 11:29:30 +0100

    ChildURLClassLoader: Cleanup and Simplify

--- src/main/java/org/lalber/tools/checkstyle/reflect/ParentLastURLClassLoader.java
+++ src/main/java/org/lalber/tools/checkstyle/reflect/ParentLastURLClassLoader.java
@@ -5,84 +5,40 @@ import java.net.URLClassLoader;
 import java.util.List;
 
 /**
- * A parent-last classloader that will try the child classloader first and then the parent.
- * This takes a fair bit of doing because java really prefers parent-first.
- *
- * For those not familiar with class loading trickery, be wary
+ * A parent-last classloader. Workaround for Java default behavior parent-first.
  */
-public class ParentLastURLClassLoader extends ClassLoader
-{
-    private ChildURLClassLoader childClassLoader;
+public class ParentLastURLClassLoader extends URLClassLoader {
+
+    private FindClassClassLoader parent;
+
+    public ParentLastURLClassLoader(List<URL> classpath) {
+        super(classpath.toArray(new URL[0]), null);
+        parent = new FindClassClassLoader(getSystemClassLoader());
+    }
+
+    @Override
+    protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        try {
+            // first we try to find a class inside the child classloader
+            return this.findClass(name);
+        }
+        catch (ClassNotFoundException e) {
+            // didn't find it, try the parent
+            return parent.loadClass(name);
+        }
+    }
 
     /**
-     * This class allows me to call findClass on a classloader
+     * Workaround for calling findClass on a Classloader
      */
-    private static class FindClassClassLoader extends ClassLoader
-    {
-        public FindClassClassLoader(ClassLoader parent)
-        {
+    private static class FindClassClassLoader extends ClassLoader {
+        public FindClassClassLoader(ClassLoader parent) {
             super(parent);
         }
 
         @Override
-        public Class<?> findClass(String name) throws ClassNotFoundException
-        {
+        public Class<?> findClass(String name) throws ClassNotFoundException {
             return super.findClass(name);
-        }
-    }
-
-    /**
-     * This class delegates (child then parent) for the findClass method for a URLClassLoader.
-     * We need this because findClass is protected in URLClassLoader
-     */
-    private static class ChildURLClassLoader extends URLClassLoader
-    {
-        private FindClassClassLoader realParent;
-
-        public ChildURLClassLoader(URL[] urls, FindClassClassLoader realParent )
-        {
-            super(urls, null);
-
-            this.realParent = realParent;
-        }
-
-        @Override
-        public Class<?> findClass(String name) throws ClassNotFoundException
-        {
-            try
-            {
-                // first try to use the URLClassLoader findClass
-                return super.findClass(name);
-            }
-            catch( ClassNotFoundException e )
-            {
-                // if that fails, we ask our real parent classloader to load the class (we give up)
-                return realParent.loadClass(name);
-            }
-        }
-    }
-
-    public ParentLastURLClassLoader(List<URL> classpath)
-    {
-        super(Thread.currentThread().getContextClassLoader());
-
-        URL[] urls = classpath.toArray(new URL[classpath.size()]);
-
-        childClassLoader = new ChildURLClassLoader( urls, new FindClassClassLoader(this.getParent()) );
-    }
-
-    @Override
-    protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException
-    {
-        try
-        {
-            // first we try to find a class inside the child classloader
-            return childClassLoader.findClass(name);
-        }
-        catch( ClassNotFoundException e )
-        {
-            // didn't find it, try the parent
-            return super.loadClass(name, resolve);
         }
     }
 }
